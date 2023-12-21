b5e28eaadf8e4ee36a2c66e8ffeb13677e986ddf
sakshi-shitole <sakshishitole1@gmail.com>
2023-12-21 15:57:34 +0530

    base is ready

---
+++ src/components/About.jsx
@@ -0,0 +1,20 @@
+import React from 'react'
+
+const About = () => {
+    return <div name="about" className='w-full h-screen bg-gradient-to-b from-gray-800 to-black text-white'>
+        <div className='max-w-screen-lg p-4 mx-auto flex flex-col justify-center w-full h-full'>
+            <div className='pb-8'>
+                <p className='text-4xl font-bold inline border-b-4 border-s-gray-500'>About</p>
+            </div>
+
+            <p className='text-xl mt-20'>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quod iure beatae hic praesentium cum voluptatum dolorem nemo rem. Optio suscipit iste minus delectus quae quia doloribus nulla ducimus veritatis sunt similique quod architecto eum maiores corrupti ab error accusamus, nesciunt modi mollitia labore maxime hic. Iusto aperiam iure quaerat eius.</p>
+
+            <br />
+
+            <p className='text-xl'>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Porro dolore deleniti dolorum neque aliquid possimus magnam mollitia, omnis voluptates vero ea molestias, itaque atque est incidunt sint laborum iusto, soluta obcaecati deserunt accusamus. Repellat harum laudantium rem repudiandae illum ipsum ex iste cum velit animi, quos molestiae nobis blanditiis ea.</p>
+
+        </div>
+    </div>
+}
+
+export default About
